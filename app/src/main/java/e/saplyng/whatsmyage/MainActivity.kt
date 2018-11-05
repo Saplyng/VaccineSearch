660aed20430631bd46d3eb56969c5790cb0e1441
Jeremy Stein <saplyng@gmail.com>
2018-11-05 17:36:59 -0600

    implemented threeTen Android Backport to fix the app, running

--- app/src/main/java/e/saplyng/whatsmyage/MainActivity.kt
+++ app/src/main/java/e/saplyng/whatsmyage/MainActivity.kt
@@ -1,15 +1,12 @@
 package e.saplyng.whatsmyage
 
-import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.util.*
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.threeten.bp.LocalDate
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidThreeTen.init(this)
         setContentView(R.layout.activity_main)
 
         idButton.setOnClickListener {
@@ -25,7 +23,7 @@ class MainActivity : AppCompatActivity() {
                 val today = LocalDate.now()
                 val parseBirth = LocalDate.parse(idDateOfBirth.text)
                 val parseVaccine = LocalDate.parse(idVaccineDate.text)
-                
+
 
                 println("working")
                 //val calculatedTime = Duration.between(parseBirth, today)
@@ -33,9 +31,9 @@ class MainActivity : AppCompatActivity() {
                 val calcuatedVaccineTime = parseBirth.until(parseVaccine)
                 val timeSinceVaccine = parseVaccine.until(today)
                 println("calculatedTime")
-                idInformation.text = "The patient is currently ${calculatedTime.years} years ${calculatedTime.months} months old" +
-                        "The patient got their vaccine when they were ${calcuatedVaccineTime.years} years ${calcuatedVaccineTime.months}" +
-                        "It has been ${timeSinceVaccine.years} years ${timeSinceVaccine.months} months since the vaccine date"
+                idInformation.text = "Patient Current Age: ${calculatedTime.years} years ${calculatedTime.months} months old"
+                idInformation2.text = "Patient Age at Vaccine:  ${calcuatedVaccineTime.years} years ${calcuatedVaccineTime.months} months old"
+                idInformation3.text = "Time Since Vaccination:  ${timeSinceVaccine.years} years ${timeSinceVaccine.months} months"
 
 
             } catch (e: Exception) {
